f54ea98e506e4b2e7d52a24cbb09ba09bb71af0f
leejung7308 <leekkzzzz@naver.com>
2019-05-29 12:51:48 +0900

    업뎃

--- key.c
+++ key.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<Windows.h>
-#include<termios.h>
+#include<conio.h>
 void load_map();
-int getch(void);
+//int getch(void);
 void left(int x, int y, int level);
 void right(int x, int y, int level);
 void up(int x, int y, int level);
 void down(int x, int y, int level);
 
-char all_map[30][30][6] = { 0 };
-char map[65][30]={0};
+char map[30][30][6] = { 0 };
 int cnt = 0;
 int level=1;
 
@@ -21,18 +20,16 @@ int main(){
 		system("clear");
 		for(i=0; i<30; i++){
 			for(j=0; j<30; j++){
-				if(all_map[i][j][level]=='@'){
+				if(map[i][j][level]=='@'){
 					pos_x=j;
 					pos_y=i;
 				}
-				if(all_map[i][j][level]=='O') cnt_O++;
-				if(all_map[i][j][level]=='$') cnt_$++;
+				if(map[i][j][level]=='O') cnt_O++;
+				if(map[i][j][level]=='$') cnt_$++;
 			}
 		}
 
-                printf("COUNT : %d\n", cnt);
-
-                //printf("%d %d\n%d %d\n",cnt_O,cnt_$,pos_x,pos_y);
+                printf("%d %d\n%d %d\n",cnt_O,cnt_$,pos_x,pos_y);
 
 		if(cnt_O!=cnt_$){
 			printf("Mismatch between box and storage count.\n");
@@ -43,12 +40,13 @@ int main(){
 			for(i=0; i<30; i++){
 				char end[30]={0};
 				for(j=0; j<30; j++){
-					printf("%c",all_map[i][j][level]);
-					end[j]=all_map[i][j][level];
+					printf("%c",map[i][j][level]);
+					end[j]=map[i][j][level];
 				}
 				printf("\n");
 				if(end[0]==0) break;
 			}
+			printf("COUNT : %d\n", cnt);
             key = getch();
 
             switch (key)
@@ -106,15 +104,21 @@ void load_map(){
 			x=0;
 		}
 		else{
-			all_map[y][x][z]=a;
+			map[y][x][z]=a;
 			x++;
 		}
 	}
 	for(i=1; i<=5; i++){
-		all_map[0][0][i]=i+48;
+		map[0][0][i]=i+48;
+	}
+	for(i=0; i<30; i++){
+		for(j=0; j<30; j++){
+			printf("%c",map[i][j][level]);
+		}
+		printf("\n");
 	}
 }
-int getch(void){
+/*int getch(void){
     int ch;
 
     struct termios buf;
@@ -133,76 +137,76 @@ int getch(void){
     tcsetattr(0, TCSAFLUSH, &save);
 
     return ch;
-}
+}*/
 void left(int x, int y, int level){
 	cnt++;
-	if(all_map[y][x-1][level]=='#') ;
+	if(map[y][x-1][level]=='#') ;
 	else{
-		if(all_map[y][x-1][level]=='$'){
-			if(all_map[y][x-2][level]=='#'||all_map[y][x-2][level]=='$') ;
+		if(map[y][x-1][level]=='$'){
+			if(map[y][x-2][level]=='#'||map[y][x-2][level]=='$') ;
 			else{
-				all_map[y][x-2][level]='$';
-				all_map[y][x-1][level]='@';
-				all_map[y][x][level]='.';
+				map[y][x-2][level]='$';
+				map[y][x-1][level]='@';
+				map[y][x][level]='.';
 			}
 		}
 		else{
-			all_map[y][x-1][level]='@';
-			all_map[y][x][level]='.';
+			map[y][x-1][level]='@';
+			map[y][x][level]='.';
 		}
 	}
 }
 void right(int x, int y, int level){
 	cnt++;
-	if(all_map[y][x+1][level]=='#') ;
+	if(map[y][x+1][level]=='#') ;
 	else{
-		if(all_map[y][x+1][level]=='$'){
-			if(all_map[y][x+2][level]=='#'||all_map[y][x+2][level]=='$') ;
+		if(map[y][x+1][level]=='$'){
+			if(map[y][x+2][level]=='#'||map[y][x+2][level]=='$') ;
 			else{
-				all_map[y][x+2][level]='$';
-				all_map[y][x+1][level]='@';
-				all_map[y][x][level]='.';
+				map[y][x+2][level]='$';
+				map[y][x+1][level]='@';
+				map[y][x][level]='.';
 			}
 		}
 		else{
-			all_map[y][x+1][level]='@';
-			all_map[y][x][level]='.';
+			map[y][x+1][level]='@';
+			map[y][x][level]='.';
 		}
 	}
 }
 void up(int x, int y, int level){
 	cnt++;
-	if(all_map[y-1][x][level]=='#') ;
+	if(map[y-1][x][level]=='#') ;
 	else{
-		if(all_map[y-1][x][level]=='$'){
-			if(all_map[y-2][x][level]=='#'||all_map[y-2][x][level]=='$') ;
+		if(map[y-1][x][level]=='$'){
+			if(map[y-2][x][level]=='#'||map[y-2][x][level]=='$') ;
 			else{
-				all_map[y-2][x][level]='$';
-				all_map[y-1][x][level]='@';
-				all_map[y][x][level]='.';
+				map[y-2][x][level]='$';
+				map[y-1][x][level]='@';
+				map[y][x][level]='.';
 			}
 		}
 		else{
-			all_map[y-1][x][level]='@';
-			all_map[y][x][level]='.';
+			map[y-1][x][level]='@';
+			map[y][x][level]='.';
 		}
 	}
 }
 void down(int x, int y, int level){
 	cnt++;
-	if(all_map[y+1][x][level]=='#') ;
+	if(map[y+1][x][level]=='#') ;
 	else{
-		if(all_map[y+1][x][level]=='$'){
-			if(all_map[y+2][x][level]=='#'||all_map[y+2][x][level]=='$') ;
+		if(map[y+1][x][level]=='$'){
+			if(map[y+2][x][level]=='#'||map[y+2][x][level]=='$') ;
 			else{
-				all_map[y+2][x][level]='$';
-				all_map[y+1][x][level]='@';
-				all_map[y][x][level]='.';
+				map[y+2][x][level]='$';
+				map[y+1][x][level]='@';
+				map[y][x][level]='.';
 			}
 		}
 		else{
-			all_map[y+1][x][level]='@';
-			all_map[y][x][level]='.';
+			map[y+1][x][level]='@';
+			map[y][x][level]='.';
 		}
 	}
 }
